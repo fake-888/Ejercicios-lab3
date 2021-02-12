647e935b7f5d629d06c4427e1b129aa70081ee63
fake-888 <56442530+fake-888@users.noreply.github.com>
2021-02-12 11:21:50 -0800

    Update ejercicio3.cpp

--- P3Lab2_MiltonVásquez/ejercicio3.cpp
+++ P3Lab2_MiltonVásquez/ejercicio3.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-using namespace std;
-#include <math.h>
-
-int main() {
-
-	cout << "Ingrese n:";
-	int n = 0;
-	int acum = 0;
-	cin >> n;
-	for(int i = 0; i <=n; i++) {
-		double acum= i - 1/(i(i+1));
-	}
-	cout << acum << endl;
-}
+ 
