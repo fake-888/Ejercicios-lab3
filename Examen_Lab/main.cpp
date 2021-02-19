e37ae5ae35a4f67c8adb92a8c29a58715d8acb9a
fake-888 <56442530+fake-888@users.noreply.github.com>
2021-02-19 14:24:21 -0800

    Add files via upload

--- Examen_Lab/main.cpp
+++ Examen_Lab/main.cpp
@@ -1,28 +1,123 @@
-#include <iostream>
-#include <conio.h>
+#include<iostream>
+#include<istream>
+#include<math.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
 
-int main()
-{
-	Cadena c1;
-	Cadena c2("Hola");
-	Cadena c3(" Mundo!");
+//Clase Resultados.  Administrador.
+using namespace std;
 
-	c1 = c2 + c3;
+class Resultados {
+	private :
+		int Njuego;
+		int ArrResultado[10];
+		string ArrEquipos[10];
 
-	cout << "Cadena: " << c1 << "\n";
-	cout << "Longitud: " << c1.tamanio() << "\n";
+	public:
+		int _Njuego,ResultadoE1, ResultadoE2;
+		string NombreEquipo1, NombreEquipo2;
+		Resultados ();
+		void DefinirPartido();
+		void DefinirGanador();
+		void MostrarPartidos();
+		void MostarInfo();
+};
 
-	Cadena c4 = c1.subcadena(5, 5);
+int ContadorNjuegos=0;
+Resultados::Resultados () {
+	Njuego = ContadorNjuegos + 1;
+	ContadorNjuegos++;
+}
 
-	cout << "Subcadena: " << c4 << "\n";
+void Resultados::DefinirGanador() {
+	cout<<"Ingrese los resultados del Partido: "<<Njuego<<endl;
+	cout<<ArrEquipos[0]<<" ";
+	cin>>ArrResultado[0];
+	cout<<ArrEquipos[1]<<" ";
+	cin>>ArrResultado[1];
+}
+
+void Resultados::DefinirPartido() {
+	string NombreEquipo1,  NombreEquipo2;
+	cout<<"Ingrese los equipos a Jugar partido: "<< Njuego <<endl;
+	cin >> ArrEquipos[0];
+	cin >> ArrEquipos[1];
+}
+
+void Resultados::MostarInfo() {
+	cout<<"Hola el resultado del Partido: "<< Njuego << " es: ";
+	cout<< ArrEquipos[0]<< " "<<ArrResultado[0]<< " - "	<< ArrEquipos[1] <<" "<< ArrResultado[1]<<endl;
+}
+
+void Resultados::MostrarPartidos() {
+	cout<<Njuego<<" "<<ArrEquipos[0]<<" Vs "<<ArrEquipos[1]<<endl;
+}
+ 
+int main() {
+	int Seleccion,Seleccion1;
+	cout<< "Liga Honduras" <<endl;
+	Resultados Partido1 =Resultados();
+	Resultados Partido2 =Resultados();
 	
-	for (int i = 0; i < c4.tamanio(); i++)
-	{
-		cout << c4.at(i) << "\n";
-	}
-	
-	
-	
-	
-	_getch();
+	do {
+		cout<<"\n1.Mostrar Resultados,\n2.Salir"<<endl;
+		cout<<"\nTu Eleccion aqui---> ";
+		cin>>Seleccion1;
+		system ("cls");
+		
+		switch(Seleccion1) {
+			case 1: {
+				do {
+					cout<<"Ingrese la accion a realizar"<<"\n1.Definir encuentros,\n2.Llenar resultados,\n3.Mostrar Resultados,\n4.salir"<<endl;
+					cout<<"\nTu Eleccion aqui---> ";
+					cin>>Seleccion;
+					system ("cls");
+
+					switch(Seleccion) {
+						case 1: {
+							Partido1.DefinirPartido();
+							Partido2.DefinirPartido();
+							system ("pause");
+							system ("cls");
+							break;
+						}
+						case 2: {
+							Partido1.DefinirGanador();
+							Partido2.DefinirGanador();
+							system ("pause");
+							system ("cls");
+							break;
+						}
+						case 3: {
+							Partido1.MostarInfo();
+							Partido2.MostarInfo();
+							system ("pause");
+							system ("cls");
+							break;
+						}
+					}
+				} while(Seleccion<4);
+				break;
+			}
+			case 2: {
+				Partido1.MostrarPartidos();
+				Partido2.MostrarPartidos();
+				system ("pause");
+				system ("cls");
+				break;
+			}
+			case 3: {
+				Partido1.MostarInfo();
+				Partido2.MostarInfo();
+				system ("pause");
+				system ("cls");
+				break;
+			}
+		}
+	} while(Seleccion1<4);
+		
+	system ("cls");
+
+	return 0;
 }
